34b309c3151bb0a47b9c75945b06b678971e2e67
silverun <imonutiy@gmail.com>
2023-02-10 14:34:07 +0300

    working login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import axios from "axios";
+import { config } from "../config";
+import { Link } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+export default function Login() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [message, setMessage] = useState(null);
 
-export default Login;
+  const loginSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(config.backendServer + "/user/login", {
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+    } catch (error) {
+      setMessage(error.response.data);
+      setTimeout(() => {
+        setMessage(null);
+      }, 2000);
+      console.log(error.response.data);
+    }
+  };
+
+  const alert = (
+    <div className="alert alert-warning mt-3" role="alert">
+      {message}
+    </div>
+  );
+
+  return (
+    <form
+      onSubmit={loginSubmitHandler}
+      className="container text-center"
+      style={{ maxWidth: 400 }}
+    >
+      <label htmlFor="emailinput" className="form-label">
+        Email
+      </label>
+      <input
+        ref={emailRef}
+        className="form-control mb-3"
+        type="email"
+        id="emailinput"
+      />
+      <label htmlFor="passwordinput" className="form-label">
+        Password
+      </label>
+      <input ref={passwordRef} className="form-control mb-3" type="password" />
+      <button type="submit" className="btn btn-primary mb-3">
+        Login
+      </button>
+      {message ? alert : null}
+      <div>
+        No account yet?{" "}
+        <Link style={{ textDecoration: "none" }} to="/register">
+          Register.
+        </Link>
+      </div>
+    </form>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const config = {
+  backendServer: "http://localhost:5000",
+};
