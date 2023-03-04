69d4a289742fc70eef8a71ef92fab18ce975030d
silverun <imonutiy@gmail.com>
2023-03-04 21:22:16 +0300

    123

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -11,7 +11,7 @@ const useAxiosPrivate = () => {
     const requestIntercept = axiosPrivate.interceptors.request.use(
       async (config) => {
         //Checking if we have access token in header
-        console.log("configheaderAuth", config.headers["Authorization"]);
+        console.log("configheaderAuth", config.headers);
         if (!config.headers["Authorization"]) {
           //if not setting it to one in state
           config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
