54db49106caa9942e38949b16bcd0d6a85eb7034
silverun <imonutiy@gmail.com>
2023-03-04 22:07:55 +0300

    IS THIS IT

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -2,19 +2,18 @@ import axios from "../api/axios";
 import useAuth from "./useAuth";
 
 const useRefreshToken = () => {
-  const { auth, setAuth } = useAuth();
+  const { setAuth } = useAuth();
 
   const refreshToken = async () => {
     const result = await axios.get("/refresh");
     console.log("new access token from refresh", result.data);
-    //
+    // was prev here
     setAuth({
       id: result.data.id,
       username: result.data.username,
       role: result.data.role,
       accessToken: result.data.accessToken,
     });
-    console.log("new auth accessToken after refresh", auth);
     return result.data.accessToken;
   };
 
