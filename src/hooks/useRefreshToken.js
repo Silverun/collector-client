21a20b1c3d23e445b6be09ab2f8203e0c8f065f4
silverun <imonutiy@gmail.com>
2023-03-04 11:33:00 +0300

    123

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -7,6 +7,13 @@ const useRefreshToken = () => {
   const refreshToken = async () => {
     const result = await axios.get("/refresh");
     setAuth((prev) => {
+      console.log({
+        ...prev,
+        id: result.data.id,
+        username: result.data.username,
+        role: result.data.role,
+        accessToken: result.data.accessToken,
+      });
       return {
         ...prev,
         id: result.data.id,
